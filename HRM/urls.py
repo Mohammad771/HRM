6f0f04527c344548d6e6b8ad00862a492e2af43f
Hasan Al-asir <Hassancf58@gmail.com>
2022-03-08 20:19:33 +0300

    Merge

--- HRM/urls.py
+++ HRM/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include(('users.urls'), namespace='users'))
+    path('',include(('users.urls'), namespace='users')),
+    path('departments/',include(('job_management.urls'), namespace='job_management'))
 ]

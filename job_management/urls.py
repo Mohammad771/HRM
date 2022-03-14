c12dee51290366078de8f28893401ffd585a0507
Hasan Al-asir <Hassancf58@gmail.com>
2022-03-14 12:14:36 +0300

    Design create_contract, viewContract & fixing register design

--- job_management/urls.py
+++ job_management/urls.py
@@ -8,5 +8,7 @@ app_name = "job_management"
 urlpatterns = [
     path('departments/', views.create_department,name='job_management/create_department'),
     path('job_titles/', views.job_titles, name='job_management/job_titles'),
+    path('create_contract/', views.create_contract, name='job_management/create_contract'),
+    path('viewContract/', views.viewContract, name='job_management/viewContract'),
 
 ]

09cf019211ec566cecb036d0bd734b763fdd5e2c
Mohammad <mohmd.mohmds@gmail.com>
2022-03-17 09:21:44 +0300

    finished developing job titles page

--- job_management/models.py
+++ job_management/models.py
@@ -19,19 +19,25 @@ class departments(models.Model):
 class job_titles(models.Model):
     job_title_id = models.AutoField(max_length=24, primary_key=True)
     job_title_department_id = models.ForeignKey(departments, on_delete=models.CASCADE, related_name="+")
-    job_title_location = models.ForeignKey(departments, on_delete=models.CASCADE, related_name="+")
-    job_title_code = models.IntegerField()
-    job_title_description = models.TextField(blank=True, null=True)
-    job_title_qualification = models.TextField(blank=True, null=True)
-    job_title_opening_position = models.CharField(max_length=264)
+    job_title_name = models.CharField(max_length=150)
+    # job_title_location = models.ForeignKey(departments, on_delete=models.CASCADE, related_name="+")
+    # job_title_code = models.IntegerField()
+    # job_title_description = models.TextField(blank=True, null=True)
+    # job_title_qualification = models.TextField(blank=True, null=True)
+    # job_title_opening_position = models.CharField(max_length=264)
     job_title_hour_price = models.IntegerField()
-    job_title_overtime_percentage = models.IntegerField()
-    job_title_available_online = models.BooleanField()
-    job_title_days_off = models.IntegerField()
-    job_title_status = models.BooleanField()
+    # job_title_overtime_percentage = models.IntegerField()
+    # job_title_available_online = models.BooleanField()
+    # job_title_days_off = models.IntegerField()
+    job_title_status = models.BooleanField(default=True)
     job_title_created_at = models.DateTimeField(auto_now=True)
-    job_title_updated_at = models.DateTimeField()
-    job_title_deleted_at = models.DateTimeField()
+    job_title_updated_at = models.DateTimeField(null = True)
+    job_title_deleted_at = models.DateTimeField(null = True)
+
+    def __str__(self):
+        return self.job_title_name
+
+
 
 class add_job_titles(models.Model):
     deptname = models.CharField(max_length=150)
--- job_management/views.py
+++ job_management/views.py
@@ -1,23 +1,25 @@
 from distutils.log import error
-from django.shortcuts import redirect, render
-from .forms import create_department_form
+from django.shortcuts import redirect, render, HttpResponse 
+from .forms import create_department_form, create_job_title_form
 from pprint import pprint
 from HRM.CRUD import *
-from .models import job_titles
+from .models import job_titles as job_titles_model
+
 
 # Create your views here.
 
 def create_department(request):
     context = {}
     context['departments'] = Read('departments')
+    print(context["departments"])
     if request.method == "POST":
-
         result = Create(request.POST, 'department')
         if result["status"] == True:
             context["success_message"] = "Department has been added 👍"
+            context['departments'] = Read('departments')
             return render(request, 'job_management/departments.html', context)
         else:
-            context["create_department_form"] = result['form']
+            context["form_errors"] = result['form']
             return render(request, 'job_management/departments.html', context)
 
     else:
@@ -25,16 +27,60 @@ def create_department(request):
         return render(request, 'job_management/departments.html', context)
 
 def job_titles(request):
-    if request.method == 'POST':
-        department = request.POST.get('deptname')
-        jobTitle = request.POST.get('jtname')
-        hourPrice = request.POST.get('hourPrice')
+    context = {}
+    Job_titles = job_titles_model.objects.all()
+    context['departments'] = Read('departments')
+    context["job_titles"] = Job_titles
+    if request.method == "POST":
 
-        var_jobTitle = add_job_titles(deptname = department, jtname = jobTitle, hourPrice = hourPrice)
-        var_jobTitle.save()
-        return render(request, 'job_management/thanks.html')
+        if request.POST['request_type'] == "update":
+            job_title_id = request.POST['job_title_id']
+            Job_title = job_titles_model.objects.get(pk=job_title_id)
+            form = create_job_title_form(request.POST, instance=Job_title)
+            if form.is_valid():
+                form.save()
+                Job_titles = job_titles_model.objects.all()
+                context["job_titles"] = Job_titles
+                return render(request, 'job_management/job_titles.html', context)
+            else:
+                context["form_errors"] = form
+                return render(request, 'job_management/job_titles.html', context)
+
+        else:
+            form = create_job_title_form(request.POST)
+            if form.is_valid():
+                form.save()
+                return render(request, 'job_management/job_titles.html', context)
+            else:
+                context["form_errors"] = form
+                return render(request, 'job_management/job_titles.html', context)
+
     else:
-        return render(request, 'job_management/job_titles.html')
+        return render(request, 'job_management/job_titles.html', context)
+
+
+def change_job_title_status(request):
+
+    job_title_id = request.POST['job_title_id']
+    switch = request.POST['switch']
+    Job_title = job_titles_model.objects.get(pk=job_title_id)
+    print(Job_title.job_title_status)
+    
+    if switch == "make_active":
+        Job_title.job_title_status = True
+    else:
+        Job_title.job_title_status = False
+
+    Job_title.save()
+    print(Job_title.job_title_status)
+ 
+    
+    html = "<html><body>Success.</body></html>" 
+    return HttpResponse(html)
+
+    
+   
+
 
 def create_contract(request):
     return render(request, 'job_management/create_contract.html')

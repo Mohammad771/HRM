5e54ef2a77c764547c36861da0f806cb386ba0da
Malek Abo Saada <mhd_malek@outlook.sa>
2022-03-21 12:59:10 +0300

    Modifying Database

--- track_performance/models.py
+++ track_performance/models.py
@@ -1,4 +1,6 @@
 ##NO EDIT HERE
+from datetime import timezone
+
 from django.db import models
 from users.models import users
 
@@ -11,9 +13,9 @@ class evaluations(models.Model):
     evaluation_interaction_rate = models.IntegerField()
     evaluation_time_rate = models.IntegerField()
     evaluation_quality_rate = models.IntegerField()
-    evaluation_created_at = models.DateTimeField()
-    evaluation_updated_at = models.DateTimeField()
-    evaluation_deleted_at = models.DateTimeField()
+    evaluation_created_at = models.DateTimeField(default=timezone.now)
+    evaluation_updated_at = models.DateTimeField(null=True, default=None, blank=True)
+    evaluation_deleted_at = models.DateTimeField(null=True, default=None, blank=True)
 
 
 class attendance(models.Model):
@@ -25,6 +27,6 @@ class attendance(models.Model):
     attendance_total_hours = models.IntegerField()
     attendance_half_day = models.BooleanField()
     attendance_working_from = models.CharField(max_length=24)
-    attendance_created_at = models.DateTimeField()
-    attendance_updated_at = models.DateTimeField()
-    attendance_deleted_at = models.DateTimeField()
+    attendance_created_at = models.DateTimeField(default=timezone.now)
+    attendance_updated_at = models.DateTimeField(null=True, default=None, blank=True)
+    attendance_deleted_at = models.DateTimeField(null=True, default=None, blank=True)
